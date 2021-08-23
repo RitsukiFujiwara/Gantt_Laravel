5eda8b699f5846058d73c5c254ced81700347a22
Fujiwara <r9.20important@icloud.com>
2021-08-23 15:00:39 +0900

    initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function store(Request $request)
+    {
+        $task = new Task();
+    }
+}
